a4a795b2da562d2228d5b684d5191e0075207bb4
judifu23 <89588103+judifu23@users.noreply.github.com>
2023-08-08 08:35:26 -0300

    Initial commit

---
+++ src/main/java/menu/ejemploversiones/EjemploVersiones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package menu.ejemploversiones;
+
+/**
+ *
+ * @author judif
+ */
+public class EjemploVersiones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
