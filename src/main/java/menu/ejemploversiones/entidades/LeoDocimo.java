8eecb1de9f6397de3376c084b36097321307c500
LeonardoAgustinDocimo <leodocimo@gmail.com>
2023-08-08 16:26:38 -0300

    Agrego nueva clase

---
+++ src/main/java/menu/ejemploversiones/entidades/LeoDocimo.java
@@ -0,0 +1,11 @@
+
+package menu.ejemploversiones.entidades;
+
+public class LeoDocimo {
+    public String nombre = "Leonardo Docimo";
+    
+  public void saludar(){
+      System.out.println("Hola soy "+ nombre);
+      
+  }
+}   
