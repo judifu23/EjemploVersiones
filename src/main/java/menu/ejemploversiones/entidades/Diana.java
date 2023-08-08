c9cdf57b8e506d83531bcf43a902c24990ad0b76
dianabere5 <136543878+dianabere5@users.noreply.github.com>
2023-08-08 16:31:37 -0300

    agregue clase Diana

---
+++ src/main/java/menu/ejemploversiones/entidades/Diana.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package menu.ejemploversiones.entidades;
+
+/**
+ *
+ * @author CLAY
+ */
+public class Diana {
+   private int dni;
+   private String nombres;
+}
