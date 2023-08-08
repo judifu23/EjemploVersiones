e27c1394e8c4d7a9eed7701cc8f0e9da27d3e895
RomiMancilla <141675896+RomiMancilla@users.noreply.github.com>
2023-08-08 16:26:34 -0300

    Agrego nueva clase

---
+++ src/main/java/menu/ejemploversiones/entidades/Romi.java
@@ -0,0 +1,18 @@
+package menu.ejemploversiones.entidades;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author telma
+ */
+public class Romi {
+    
+    private int id;
+    private String nombre;
+    
+}
